4a687d2bec4b52a7bd1ce8885d7b9f4bd0ca8ac9
Dominika Smorąg <dominikasmorag@gmail.com>
2025-04-07 03:02:04 +0200

    added function to show saved notes and display them after adding new one

--- js/notes.js
+++ js/notes.js
@@ -3,13 +3,41 @@
     const saveNotesButton = document.getElementById('saveNotesButton');
     const saveStatus = document.getElementById('saveStatus');
 
-    if (localStorage.getItem('notepadContent')) {
-        notepad.value = localStorage.getItem('notepadContent');
-    }
+    displayNotes();
 
     saveNotesButton.addEventListener('click', function () {
-        localStorage.setItem('notepadContent', notepad.value);
-        saveStatus.textContent = 'Notes saved!';
-        setTimeout(() => { saveStatus.textContent = ''; }, 2000);
+        const note = notepad.value.trim();
+        if (note) {
+            const notes = getNotes();
+            notes.push(note);
+            saveNotes(notes)
+        
+            saveStatus.textContent = 'Notes saved!';
+            setTimeout(() => { saveStatus.textContent = ''; }, 2000);
+
+            notepad.value = "";
+            displayNotes();
+            console.log(notes);
+        }
     });
 });
+
+
+function getNotes() {
+    const notes = localStorage.getItem('notepadContent')
+    return notes ? JSON.parse(notes) : [];
+}
+
+function saveNotes(notes) {
+    localStorage.setItem('notepadContent', JSON.stringify(notes));
+}
+
+function displayNotes() {
+    const notes = getNotes();
+    const notesList = document.getElementById('notes-list');
+
+    notesList.innerHTML = '';
+    notes.forEach(note => {
+        notesList.innerHTML += `<li>${note}</li>`;
+    })
+}
