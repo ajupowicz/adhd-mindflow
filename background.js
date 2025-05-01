ab50edc6ea574c5c2cbdf4a0600ae6886c61c86a
Dominika Smorąg <dominikasmorag@gmail.com>
2025-05-01 11:57:21 +0200

    tried to change default sound volumne - not working yet

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 
 let whiteNoise = new Audio("sounds/white-noise.mp3");
 whiteNoise.loop = true;
+whiteNoise.volume = 0.5;
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.command === "playWhiteNoise") {
