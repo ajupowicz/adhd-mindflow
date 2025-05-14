5954f901687b295a69676a405f5532d205bc3b61
Dominika Smorąg <dominikasmorag@gmail.com>
2025-05-14 23:26:34 +0200

    toggle white noise instead of playing .mp3 file

--- background.js
+++ background.js
@@ -1,15 +1,26 @@
 
-let whiteNoise = new Audio("sounds/white-noise.mp3");
-whiteNoise.loop = true;
-whiteNoise.volume = 0.5;
+// let whiteNoise = new Audio("sounds/white-noise.mp3");
+// whiteNoise.loop = true;
+// whiteNoise.volume = 0.5;
 
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     if (request.command === "playWhiteNoise") {
+//         whiteNoise.play();
+//         sendResponse({ status: "playing" });
+//     }
+//     if (request.command === "pauseWhiteNoise") {
+//         whiteNoise.pause();
+//         sendResponse({ status: "paused" });
+//     }
+// });
+
+//dodane
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.command === "playWhiteNoise") {
-        whiteNoise.play();
-        sendResponse({ status: "playing" });
+    if (request.command === "toggleWhiteNoise") {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        if (tabs[0]) {
+          chrome.tabs.sendMessage(tabs[0].id, { command: "toggleWhiteNoise" });
+        }
+      });
     }
-    if (request.command === "pauseWhiteNoise") {
-        whiteNoise.pause();
-        sendResponse({ status: "paused" });
-    }
-});
+  });
